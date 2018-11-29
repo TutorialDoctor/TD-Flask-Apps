4f0e20ed85d872029ab57a883e755baa1c4538c4
Tutorial Doctor <thetutorialdoctor@gmail.com>
2018-11-28 22:16:43 -0500

    DB update
    Added default values to db.

--- Apps/Flask_Restless/app1.py
+++ Apps/Flask_Restless/app1.py
@@ -7,9 +7,9 @@ app.config.from_pyfile('config1.cfg')
 db = SQLAlchemy(app)
 
 # Create DB
-@app.before_first_request
-def create_tables():
-    db.create_all() # Create db
+#@app.before_first_request
+#def create_tables():
+    #db.create_all() # Create db
 
 class Parent(db.Model):
     id = db.Column(db.Integer,primary_key=True)
