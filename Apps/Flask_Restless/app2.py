8db6ce3e3674aedf364f6e05611193adeb41b366
Tutorial Doctor <thetutorialdoctor@gmail.com>
2018-11-28 22:06:34 -0500

    Two examples
    Added one more example. app1 and app2 now exist.

--- Apps/Flask_Restless/app2.py
+++ Apps/Flask_Restless/app2.py
@@ -3,9 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restless import APIManager
 
 app = Flask(__name__)
-app.config.from_pyfile('config.cfg')
+app.config.from_pyfile('config2.cfg')
 db = SQLAlchemy(app)
 
+# Create DB
+@app.before_first_request
+def create_tables():
+    db.create_all() # Create db
+
+    
 class Person(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(20))
@@ -17,7 +23,7 @@ class Pet(db.Model):
     owner_id = db.Column(db.Integer,db.ForeignKey('person.id'))
 
 manager = APIManager(app,flask_sqlalchemy_db=db)
-manager.create_api(Person)
+manager.create_api(Person,methods=['GET','POST','DELETE','PUT'])
 manager.create_api(Pet)
 
 
